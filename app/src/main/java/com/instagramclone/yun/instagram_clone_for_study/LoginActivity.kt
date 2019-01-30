202faf0e334b2fd8cdba2dd3233d1f970fc50c9f
Yunmonkey <yts0646@naver.com>
2019-01-30 10:23:46 +0900

    INIT

---
+++ app/src/main/java/com/instagramclone/yun/instagram_clone_for_study/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.instagramclone.yun.instagram_clone_for_study
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
