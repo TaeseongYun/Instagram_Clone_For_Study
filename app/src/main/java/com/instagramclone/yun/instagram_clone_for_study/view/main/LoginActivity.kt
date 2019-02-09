2d84e2f323a09a54981e1e3d150f12e78de02e29
Yunmonkey <yts0646@naver.com>
2019-02-09 20:04:25 +0900

    Update[FacebookLogin]

--- app/src/main/java/com/instagramclone/yun/instagram_clone_for_study/view/main/LoginActivity.kt
+++ app/src/main/java/com/instagramclone/yun/instagram_clone_for_study/view/main/LoginActivity.kt
@@ -5,17 +5,25 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.instagramclone.yun.instagram_clone_for_study.R
 import kotlinx.android.synthetic.main.activity_login.*
 import com.instagramclone.yun.instagram_clone_for_study.util.*
+import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -23,9 +31,14 @@ class LoginActivity : AppCompatActivity() {
     val GOOGLE = 9001
     private var auth: FirebaseAuth? = null
     lateinit var googleSignInClient: GoogleSignInClient
+    lateinit var callbackManager: CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        auth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
@@ -34,7 +47,7 @@ class LoginActivity : AppCompatActivity() {
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        auth = FirebaseAuth.getInstance()
+
 
         signIn_button.setOnClickListener {
             createUserAndLogin()
@@ -42,7 +55,9 @@ class LoginActivity : AppCompatActivity() {
         googleLogin_button.setOnClickListener {
             googleLogin()
         }
-//        facebookLogin_button.setOnClickListener {  }
+        facebookLogin_button.setOnClickListener {
+            facebookLogin()
+        }
 
 
 
@@ -91,8 +106,10 @@ class LoginActivity : AppCompatActivity() {
         startActivityForResult(googleSignInIntent, GOOGLE)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) { //모든 데이터가 모이는곳
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
         if(resultCode == Activity.RESULT_OK) {
             if(requestCode == GOOGLE) {
                 val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
@@ -109,10 +126,37 @@ class LoginActivity : AppCompatActivity() {
         auth?.signInWithCredential(cridential)?.addOnCompleteListener {
             if(it.isSuccessful) {
                 myMakeText(this, R.string.signin_complete, Toast.LENGTH_LONG)
+                movePage(auth?.currentUser)
             }
         }
     }
 
+    fun facebookLogin() {
+        LoginManager
+                .getInstance()
+                .logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+        LoginManager
+                .getInstance()
+                .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                handleFacebookAccessToken(result?.accessToken)
+            }
+
+            override fun onCancel() {
+
+            }
+
+            override fun onError(error: FacebookException?) {
+
+            }
+        })
+    }
+
+    fun handleFacebookAccessToken(token: AccessToken?) {
+        var cridential = token?.token?.let { FacebookAuthProvider.getCredential(it) }
+        cridential?.let { auth?.signInWithCredential(it) }
+        movePage(auth?.currentUser)
+    }
     override fun onResume() {
         super.onResume()
         movePage(auth?.currentUser)
