419c34489ea58b6f58317433f795d6e699f03da1
Yunmonkey <yts0646@naver.com>
2019-02-18 22:49:03 +0900

    Update[UserFragment]

---
+++ app/src/main/java/com/instagramclone/yun/instagram_clone_for_study/model/FollowDTO.kt
@@ -0,0 +1,11 @@
+package com.instagramclone.yun.instagram_clone_for_study.model
+
+data class FollowDTO(
+        var followerCount: Int = 0,
+        //제 3자가 나를 팔로잉 하는지
+        var follwers: MutableMap<String?, Boolean> = HashMap(),
+
+        var followingCount: Int = 0,
+        //내가 누구를 follwing 하는지
+        var follwings: MutableMap<String?, Boolean> = HashMap()
+)
--- app/src/main/java/com/instagramclone/yun/instagram_clone_for_study/view/main/UserFragment.kt
+++ app/src/main/java/com/instagramclone/yun/instagram_clone_for_study/view/main/UserFragment.kt
@@ -5,28 +5,29 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutCompat
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.text.BoringLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.instagramclone.yun.instagram_clone_for_study.R
 import com.instagramclone.yun.instagram_clone_for_study.model.ContentDTO
+import com.instagramclone.yun.instagram_clone_for_study.model.FollowDTO
 import kotlinx.android.synthetic.main.fragment_user.*
 import kotlinx.android.synthetic.main.fragment_user.view.*
 
 class UserFragment : Fragment() {
 
-
-    val uid = FirebaseAuth.getInstance().currentUser?.uid
+    private val firestore: FirebaseFirestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+    //현재 나의 Uid
+    val currentUserUid = FirebaseAuth.getInstance().currentUser?.uid
+    var uid: String? = null
     companion object {
         private val PICK_PROFILE_FROM_ALBUM = 10
 
@@ -37,26 +38,93 @@ class UserFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             LayoutInflater.from(inflater.context).inflate(R.layout.fragment_user,container,false)
                     .apply {
+                        if(arguments != null) {
+                            uid = arguments?.getString("destinationUid")
+                        }
                         account_iv_profile.setOnClickListener {
                             Intent(Intent.ACTION_PICK).apply {
                                 type = "image/*"
                                 activity?.startActivityForResult(this, PICK_PROFILE_FROM_ALBUM)
                             }
                         }
+                        account_btn_follow_signout.seton
                         account_recyclerview.adapter = UserFragmentRecyclerview()
                         account_recyclerview.layoutManager = GridLayoutManager(activity, 3)
                     }
 
+    fun requestFollow() {
+        var tsDocFollowing = currentUserUid?.let {
+            firestore.collection("user").document(it)
+        }
+        firestore.runTransaction {
+            transaction ->
+            var followDTO = tsDocFollowing?.let { transaction.get(it).toObject(FollowDTO::class.java) }
+            if(followDTO == null) {
+                //아무도 팔로잉 하지 않았을 경우
+                followDTO = FollowDTO().apply {
+                    followerCount = 1
+                    follwings[uid] = true
+
+                    tsDocFollowing?.let { tsDoc -> followDTO?.let { follow -> transaction.set(tsDoc, follow) } }
+                }
+                return@runTransaction
+            }
+
+            if(followDTO.follwings.containsKey(uid)) {
+                //내 아이디가 제 3자를 이미 팔로잉 하고 있을 경우
+                //follow를 취소하는 코드 (이미 팔로잉 하고있으니까) -> 제 3자가 나를 팔로워 취소한다.
+                followDTO.followingCount -= 1
+                followDTO.follwings.remove(uid)
+            }else {
+                //내가 제 3자 팔로잉 하지 않았을 경우 -> 제 3자가 나를 팔로워 한다.
+                followDTO.followingCount += 1
+                followDTO.follwings[uid] = true
+            }
+            tsDocFollowing?.let { transaction.set(it, followDTO!!) }
+            return@runTransaction
+        }
+        var tsDocFollwer = uid?.let { firestore.collection("users").document(it) }
+        firestore.runTransaction {
+            transaction ->
+            var followDTO = tsDocFollowing?.let { transaction.get(it).toObject(FollowDTO::class.java) }
+
+            if(followDTO == null) {
+                //아무도 팔로우 하지 않았을 경우
+                followDTO = FollowDTO().apply {
+                    followerCount = 1
+                    follwers[currentUserUid] = true
+
+                    tsDocFollwer?.let { tsDocFollwer -> followDTO?.let{ followDTO -> transaction.set(tsDocFollwer, followDTO) } }
+                }
+                return@runTransaction
+            }
+            //제 3자의 유저를 내가 필로잉 하고 있을 경우 -> 내가 제3자 팔로우를 취소한다.
+            if(followDTO?.follwers?.containsKey(currentUserUid)!!) {
+                followDTO!!.followerCount -= 1
+                followDTO!!.follwers.remove(currentUserUid)
+
+                tsDocFollwer?.let { tsDocFollwer -> followDTO?.let{ followDTO -> transaction.set(tsDocFollwer, followDTO) } }
+            }else {
+                //제 3자를 내가 팔로워 하지 않았을 경우 -> 내가 제 3자 팔로우를 하겠다.
+                followDTO!!.followerCount += 1
+                followDTO!!.follwers[currentUserUid] = true
+
+            }
+            tsDocFollwer?.let { tsDocFollwer -> followDTO?.let{ followDTO -> transaction.set(tsDocFollwer, followDTO) } }
+            return@runTransaction
+        }
+    }
     override fun onResume() {
         super.onResume()
         getProfileImage()
     }
     fun getProfileImage() {
-        uid?.let { FirebaseFirestore.getInstance().collection("profileImages").document(it).addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
-            if(documentSnapshot.data != null) {
-                val url = documentSnapshot.data["image"]
-                Glide.with(activity).load(url).apply ( RequestOptions().circleCrop() ).into(account_iv_profile)
-            }
+        currentUserUid?.let {
+            firestore.collection("profileImages").document(it).addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+                if(documentSnapshot.data != null) {
+                    val url = documentSnapshot.data["image"]
+                    Glide.with(activity).load(url).apply ( RequestOptions().circleCrop() ).into(account_iv_profile)
+                }
             }
         }
     }
@@ -64,7 +132,7 @@ class UserFragment : Fragment() {
         val contnetDTO: ArrayList<ContentDTO>
         init {
             contnetDTO = ArrayList()
-            FirebaseFirestore.getInstance().collection("image").whereEqualTo("uid", uid).addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            firestore.collection("image").whereEqualTo("uid", currentUserUid).addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                 for(snapshot in querySnapshot.documents) {
                     contnetDTO.add(snapshot.toObject(ContentDTO::class.java))
                 }
